17dce3f6cfdee171b15206808911ec5e031efcce
Linus Liang <linus.h.liang@gmail.com>
2015-11-08 12:23:01 -0500

    sped up largest palindrome with number of digits

--- ProjectEuler.playground/Pages/Largest Palindrome Product.xcplaygroundpage/Contents.swift
+++ ProjectEuler.playground/Pages/Largest Palindrome Product.xcplaygroundpage/Contents.swift
@@ -64,25 +64,58 @@ extension Int {
         }
         return self == (reversedNum / 10)
     }
+    
+    // added to prevent casting to double and back 
+    func toThePowerOf(num: Int) -> Int {
+        var result = self
+        for _ in 0..<num {
+            result *= self
+        }
+        return result
+    }
 }
 
 
 
+
+//func largestPalindromWithXNumberOfDigits(numberOfDigits: Int) -> Int {
+//    let largest = Int((0..<numberOfDigits).map{_ in "9"}.joinWithSeparator(""))!
+//    var results = [Int]()
+//    for var i = largest; i >= 10; i-- {
+//        for var j = largest; j >= 10; j-- {
+//            if (i * j).isPalindrome() {
+//                results.append(i * j)
+//            }
+//        }
+//    }
+//    return results.sort { $0 > $1 }.first!   // n log(n) runtime 
+//}
+
+
+// space complexity : O(1)
+// runTime : ( O(n^2) ) actual runTime will be less.
 
 func largestPalindromWithXNumberOfDigits(numberOfDigits: Int) -> Int {
     let largest = Int((0..<numberOfDigits).map{_ in "9"}.joinWithSeparator(""))!
-    var results = [Int]()
-    for var i = largest; i >= 10; i-- {
-        for var j = largest; j >= 10; j-- {
-            if (i * j).isPalindrome() {
-                results.append(i * j)
+    let lowerBound = (numberOfDigits > 1) ? 10.toThePowerOf(numberOfDigits - 2) : 1
+    var maxNum = 0
+    for var i = largest; i >= lowerBound ; i-- {
+        for var j = largest; j >= lowerBound; j-- {
+            switch i * j {
+            case let product where product < maxNum:             // since j decreases, the product only gets
+                break                                            // smaller, so we can skip those
+            case let product where product.isPalindrome():
+                maxNum = product
+            default:
+                continue
             }
         }
     }
-    return results.sort { $0 > $1 }.first!
+    return maxNum
 }
 
+
 // uncomment the line below to execute
-// largestPalindromWithXNumberOfDigits(3)
+//largestPalindromWithXNumberOfDigits(3)
 
 //: [Next](@next)
